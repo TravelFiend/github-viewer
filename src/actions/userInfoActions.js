9e9673ebfc40d1ecd40f6c73f43350393174b73b
TravelFiend <mikegrace274@gmail.com>
2020-02-14 15:30:02 -0800

    user info action in place

---
+++ src/actions/userInfoActions.js
@@ -0,0 +1,14 @@
+import getGithubStuff from '../services/getGithubStuff';
+
+export const FETCH_INFO_LOADING = 'FETCH_INFO_LOADING';
+export const FETCH_USER_INFO = 'FETCH_USER_INFO';
+export const fetchUserInfo = loginName => dispatch => {
+  dispatch({ type: FETCH_INFO_LOADING });
+  return getGithubStuff(loginName)
+    .then(info => {
+      dispatch({
+        type: FETCH_USER_INFO,
+        payload: info
+      });
+    });
+};
