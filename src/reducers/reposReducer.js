5a96d369b42ace8477c9a66655d5f400e55ced54
TravelFiend <mikegrace274@gmail.com>
2020-02-15 21:45:03 -0800

    repos reducer and test written and passing

--- src/reducers/reposReducer.js
+++ src/reducers/reposReducer.js
@@ -9,6 +9,8 @@ export default function reducer(state = initialState, action){
   switch(action.type){
     case FETCH_REPOS_LOADING:
       return { ...state, loading: true };
+    case FETCH_REPOS:
+      return { ...state, loading: false, repos: action.payload };
     default: return state;
   }
 }
